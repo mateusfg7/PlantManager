9077b4a0d0a0f914911ab6e460e5bf370afe094e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-05 12:09:48 -0300

    change main page to Welcome to UserIdentification
    - scope: App

--- App.tsx
+++ App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import AppLoading from 'expo-app-loading'
+import AppLoading from 'expo-app-loading';
 import {
   useFonts,
   Jost_400Regular,
-  Jost_600SemiBold
-} from '@expo-google-fonts/jost'
+  Jost_600SemiBold,
+} from '@expo-google-fonts/jost';
 
 import { Welcome } from './src/pages/Welcome';
+import { UserIdentification } from './src/pages/UserIdentification';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
-    Jost_600SemiBold
-  })
+    Jost_600SemiBold,
+  });
 
-  if (!fontsLoaded)
-    return <AppLoading />
+  if (!fontsLoaded) return <AppLoading />;
 
-  return <Welcome />
-
-
+  return <UserIdentification />;
 }
