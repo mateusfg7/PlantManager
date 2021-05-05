8525ff4379015efe8416e09f08a36948f4df457f
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-05 10:12:17 -0300

    bring back Button component to Welcome page as a element

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {
   Text,
-  View,
   Image,
   TouchableOpacity,
   StyleSheet,
@@ -11,23 +10,29 @@ import {
 } from 'react-native';
 
 import wateringImg from '../assets/watering.png';
-import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 export function Welcome() {
   return (
     <SafeAreaView style={styles.container}>
+
       <Text style={styles.title}>
         Gerencie {'\n'}
         suas plantas {'\n'}
         de forma fácil
       </Text>
+
       <Image source={wateringImg} style={styles.image} />
+
       <Text style={styles.subtitle}>
         Não esqueça mais de regrar sua plantas. Nós cuidamos de lembrar você
         sempre que precisar.
       </Text>
-      <Button title='>' />
+
+      <TouchableOpacity style={styles.button} activeOpacity={0.7}>
+        <Text style={styles.buttonText}> {'>'} </Text>
+      </TouchableOpacity>
+
     </SafeAreaView>
   );
 }
@@ -35,8 +40,8 @@ export function Welcome() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'space-between',
     alignItems: 'center',
+    justifyContent: 'space-around',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
   },
   title: {
@@ -44,6 +49,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
+    marginTop: 38
   },
   subtitle: {
     textAlign: 'center',
@@ -51,9 +57,21 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     color: colors.heading,
   },
-
   image: {
     width: 292,
     height: 284,
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginRight: 10,
+    height: 56,
+    width: 56,
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24,
   },
 });
