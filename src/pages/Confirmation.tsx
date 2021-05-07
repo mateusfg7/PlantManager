8edb721860ca79befcf92323f1e9310ab605f498
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-07 11:51:48 -0300

    connect Confirmation page to routes

--- src/pages/Confirmation.tsx
+++ src/pages/Confirmation.tsx
@@ -15,6 +15,12 @@ import colors from '../styles/colors';
 import fonts from '../styles/fonts';
 
 export function Confirmation() {
+  const navigation = useNavigation();
+
+  function handleMoveOn() {
+    navigation.navigate('PlantSelect');
+  }
+
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.content}>
@@ -28,7 +34,7 @@ export function Confirmation() {
         </Text>
 
         <View style={styles.footer}>
-          <Button title='Começar' />
+          <Button title='Começar' onPress={handleMoveOn} />
         </View>
       </View>
     </SafeAreaView>
