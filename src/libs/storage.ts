2d64397efd47761f0e463e010cf100dbf81de53e
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-10 10:56:05 -0300

    create loadPlants function on storage lib

--- src/libs/storage.ts
+++ src/libs/storage.ts
@@ -43,3 +43,31 @@ export async function savePlant(plant: PlantProps): Promise<void> {
     throw new Error(err);
   }
 }
+
+export async function loadPlant(): Promise<PlantProps[]> {
+  try {
+    const data = await AsyncStorage.getItem('@plantmanager:plants');
+    const plants = data ? (JSON.parse(data) as StoragePlantProps) : {};
+
+    const plantsSorted = Object.keys(plants)
+      .map((plant) => {
+        return {
+          ...plants[plant].data,
+          hour: format(
+            new Date(plants[plant].data.dateTimeNotification),
+            'HH:mm'
+          ),
+        };
+      })
+      .sort((a, b) =>
+        Math.floor(
+          new Date(a.dateTimeNotification).getTime() / 1000 -
+            Math.floor(new Date(b.dateTimeNotification).getTime() / 1000)
+        )
+      );
+
+    return plantsSorted;
+  } catch (err) {
+    throw new Error(err);
+  }
+}
