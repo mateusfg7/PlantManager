b54bb7364615adbec15dc8927a1f6238e1fe4b6a
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2021-05-10 16:15:57 -0300

    fix stack route instead tab routes on stack.routes

--- src/routes/stack.routes.tsx
+++ src/routes/stack.routes.tsx
@@ -5,7 +5,6 @@ import { Welcome } from '../pages/Welcome';
 import { UserIdentification } from '../pages/UserIdentification';
 import { Confirmation } from '../pages/Confirmation';
 import { PlantSave } from '../pages/PlantSave';
-import { MyPlants } from '../pages/MyPlants';
 
 import AuthRoutes from './tab.routes';
 
@@ -30,7 +29,7 @@ const AppRoutes: React.FC = () => (
     <stackRoutes.Screen name='Confirmation' component={Confirmation} />
     <stackRoutes.Screen name='PlantSelect' component={AuthRoutes} />
     <stackRoutes.Screen name='PlantSave' component={PlantSave} />
-    <stackRoutes.Screen name='MyPlants' component={MyPlants} />
+    <stackRoutes.Screen name='MyPlants' component={AuthRoutes} />
   </stackRoutes.Navigator>
 );
 
